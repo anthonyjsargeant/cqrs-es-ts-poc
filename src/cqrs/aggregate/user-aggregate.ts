dbb7175727368fcae2668130418eb62febf1364c
Anthony Sargeant <anthonyjsargeant@live.com>
2023-02-13 17:34:27 +0000

    Added the Java code as a comment to aide the translation

--- src/cqrs/aggregate/user-aggregate.ts
+++ src/cqrs/aggregate/user-aggregate.ts
@@ -13,4 +13,67 @@ export class UserAggregate {
     this.eventStore.addEvent(command.userId, userCreatedEvent);
     return [ userCreatedEvent ];
   }
+
+  //   public List<Event> handleUpdateUserCommand(UpdateUserCommand command) {
+  //     User user = recreateUserState(writeRepository, command.getUserId());
+  //     List<Event> events = new ArrayList<>();
+
+  //     List<Contact> contactsToRemove = user.getContacts()
+  //         .stream()
+  //         .filter(contact -> !command.getContacts().contains(contact))
+  //         .toList();
+
+  //     for (Contact contact : contactsToRemove) {
+  //         UserContactRemovedEvent userContactRemovedEvent = new UserContactRemovedEvent(contact.getType(),
+  //             contact.getDetail());
+  //         events.add(userContactRemovedEvent);
+  //         writeRepository.addEvent(command.getUserId(), userContactRemovedEvent);
+  //     }
+
+  //     addContacts(command, user, events);
+  //     removeAddresses(command, user, events);
+  //     addAddresses(command, user, events);
+
+  //     return events;
+  // }
+
+  // private void removeAddresses(UpdateUserCommand command, User user, List<Event> events) {
+  //     List<Address> addressesToRemove = user.getAddresses()
+  //         .stream()
+  //         .filter(address -> !user.getAddresses().contains(address))
+  //         .toList();
+
+  //     for (Address address : addressesToRemove) {
+  //         UserAddressRemovedEvent userAddressRemovedEvent =
+  //             new UserAddressRemovedEvent(address.getCity(), address.getCounty(), address.getPostcode());
+  //         events.add(userAddressRemovedEvent);
+  //         writeRepository.addEvent(command.getUserId(), userAddressRemovedEvent);
+  //     }
+  // }
+
+  // private void addAddresses(UpdateUserCommand command, User user, List<Event> events) {
+  //     List<Address> addressesToAdd = command.getAddresses()
+  //         .stream()
+  //         .filter(address -> !user.getAddresses().contains(address))
+  //         .toList();
+
+  //     for (Address address : addressesToAdd) {
+  //         UserAddressAddedEvent userAddressAddedEvent
+  //             = new UserAddressAddedEvent(address.getCity(), address.getCounty(), address.getPostcode());
+  //         events.add(userAddressAddedEvent);
+  //         writeRepository.addEvent(command.getUserId(), userAddressAddedEvent);
+  //     }
+  // }
+
+  // private void addContacts(UpdateUserCommand command, User user, List<Event> events) {
+  //     List<Contact> contactsToAdd = command.getContacts().stream()
+  //         .filter(contact -> !user.getContacts().contains(contact))
+  //         .toList();
+  //     for (Contact contact : contactsToAdd) {
+  //         UserContactAddedEvent contactAddedEvent = new UserContactAddedEvent(contact.getType(),
+  //             contact.getDetail());
+  //         events.add(contactAddedEvent);
+  //         writeRepository.addEvent(command.getUserId(), contactAddedEvent);
+  //     }
+
 }
